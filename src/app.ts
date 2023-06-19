70758f171bbc9212bab132b6d0d422e443e47c36
PedroCfF <Pedro.Cienfuegos@solera.com>
2023-06-19 12:41:39 +0200

    app dockerized

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import apartmentsRouter from "./routes/apartmentsRouter";
 
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
   console.log("App listening on port " + port);
